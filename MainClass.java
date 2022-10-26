cbf49f3ca470615ae635c5050772fad1a639a59c
Alex Davis <113662493+alexandermdavis@users.noreply.github.com>
2022-10-25 20:17:29 -0500

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,16 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Rectangle r1 = new Rectangle(5, 8);
+		r1.draw();
+		
+		System.out.println();
+		
+		Rectangle r2 = new Rectangle(8, 5);
+		r2.draw();
+		
+	}
+	
+}
